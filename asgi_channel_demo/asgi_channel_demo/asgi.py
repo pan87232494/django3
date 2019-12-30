1c9afce30ae87fa6d4e70051507daef13ba37e3c
panbingrui <pan_bingrui@xjeralabs.com>
2019-12-30 15:39:25 +0800

    add remote debug configuration

--- asgi_channel_demo/asgi_channel_demo/asgi.py
+++ asgi_channel_demo/asgi_channel_demo/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asgi_channel_demo.settings')
 
 application = get_asgi_application()
+
+from . import settings
+if settings.DEBUG:
+    import ptvsd
+    ptvsd.enable_attach(address=('127.0.0.1', 3000), redirect_output=True)
+
+    # Pause the program until a remote debugger is attached
+    ptvsd.wait_for_attach()
